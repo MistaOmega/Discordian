dfe694da0cad01cf3db04972cd84e877ca27285e
Jack Nash <jack.nash6@gmail.com>
2021-02-07 13:09:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const {Client} = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({ partials: ['MESSAGE'] });
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+client.on('ready', onReady);
+client.on('messageReactionAdd', addRole);
+client.on('messageReactionRemove', delRole);
+
+client.login(process.env.BOT_TOKEN)
+
+async function onReady(){
+    let roleChannel = client.channels.cache.get(config.channel)
+    try{
+        await roleChannel.messages.fetch();
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    roleChannel.messages.fetch().then(messages =>{
+        config.messageID = messages.first().id;
+        console.log(`Watching over '${config.messageID}`)
+    })
+
+    await client.user.setPresence({
+        activity:{
+            name:"over all",
+            type: "WATCHING"
+        },
+        status: 'online'
+    });
+
+}
+
+/**
+ * Event Handler when a message is reacted to
+ * @param reaction reacted message
+ * @param user user who reacted
+ * @returns {Promise<void>}
+ */
+async function addRole({message, emoji}, user){
+    console.log(user.id);
+
+    //ignore bots, and return if the messageID assigned earlier isn't the message being reacted to
+    if (user.bot || message.id !== config.messageID) {
+        return;
+    }
+
+    if (message.partial) {
+        try {
+            await message.fetch();
+        } catch (e) {
+            console.error('Error on fetching message, partial received', e);
+            return;
+        }
+    }
+
+    const { guild: {members, roles} } = message;
+    const member = await members.fetch(user.id)
+    const role = roles.cache.get(config.roles[emoji.name]);
+
+    if (role) {
+        try {
+            member.roles.add(role.id);
+        } catch (e) {
+            console.error('Error adding role', e);
+
+        }
+    }
+}
+
+
+async function delRole({message, emoji}, user){
+    console.log(user.id);
+
+    //ignore bots, and return if the messageID assigned earlier isn't the message being reacted to
+    if (user.bot || message.id !== config.messageID) {
+        return;
+    }
+
+    if (message.partial) {
+        try {
+            await message.fetch();
+        } catch (e) {
+            console.error('Error on fetching message, partial received', e);
+            return;
+        }
+    }
+
+    const { guild: {members, roles} } = message;
+    const member = await members.fetch(user.id)
+    const role = roles.cache.get(config.roles[emoji.name]);
+
+    if (role) {
+        try {
+            member.roles.remove(role.id);
+        } catch (e) {
+            console.error('Error adding role', e);
+
+        }
+    }
+
+
+}
+
